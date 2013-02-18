f5369f493ebc6a776bbca20c3a1392cf93d26e65
lanuser <lanuser@lanuser.ru>
2013-02-18 09:30:14 +0400

    db

--- system/language/ru/db_lang.php
+++ system/language/ru/db_lang.php
@@ -1,16 +1,16 @@
 <?php
 
-$lang['db_invalid_connection_str'] = 'Unable to determine the database settings based on the connection string you submitted.';
-$lang['db_unable_to_connect'] = 'Unable to connect to your database server using the provided settings.';
-$lang['db_unable_to_select'] = 'Unable to select the specified database: %s';
-$lang['db_unable_to_create'] = 'Unable to create the specified database: %s';
-$lang['db_invalid_query'] = 'The query you submitted is not valid.';
-$lang['db_must_set_table'] = 'You must set the database table to be used with your query.';
-$lang['db_must_set_database'] = 'You must set the database name in your database config file.';
-$lang['db_must_use_set'] = 'You must use the "set" method to update an entry.';
-$lang['db_must_use_index'] = 'You must specify an index to match on for batch updates.';
-$lang['db_batch_missing_index'] = 'One or more rows submitted for batch updating is missing the specified index.';
-$lang['db_must_use_where'] = 'Updates are not allowed unless they contain a "where" clause.';
+$lang['db_invalid_connection_str'] = 'Невозможно определить настройки базы данных на основании строки соединения.';
+$lang['db_unable_to_connect'] = 'Невозможно подключиться к серверу базы данных.';
+$lang['db_unable_to_select'] = 'Невозможно выбрать указанную базу данных: %s';
+$lang['db_unable_to_create'] = 'Невозможно создать указанную базу данных: %s';
+$lang['db_invalid_query'] = 'Отправленный запрос не верный.';
+$lang['db_must_set_table'] = 'Вы должны указать таблицу базы данных для выполнения с вашим запросом.';
+$lang['db_must_set_database'] = 'Вы должны указать имя базы данных в файле конфигурации базы данных.';
+$lang['db_must_use_set'] = 'Вы должны использовать метод "set" для обновления записи.';
+$lang['db_must_use_index'] = 'Вы должны указать индекс для сопоставления пакетного обновления.';
+$lang['db_batch_missing_index'] = 'Один или несколько ячеек представленных для пакетного обновления отсутствует по указанному индексу.';
+$lang['db_must_use_where'] = 'Обновления не допускаются, если они не содержат условия "where".';
 $lang['db_del_must_use_where'] = 'Deletes are not allowed unless they contain a "where" or "like" clause.';
 $lang['db_field_param_missing'] = 'To fetch fields requires the name of the table as a parameter.';
 $lang['db_unsupported_function'] = 'This feature is not available for the database you are using.';
@@ -21,10 +21,10 @@ $lang['db_unsuported_compression'] = 'The file compression format you chose is n
 $lang['db_filepath_error'] = 'Unable to write data to the file path you have submitted.';
 $lang['db_invalid_cache_path'] = 'The cache path you submitted is not valid or writable.';
 $lang['db_table_name_required'] = 'A table name is required for that operation.';
-$lang['db_column_name_required'] = 'A column name is required for that operation.';
-$lang['db_column_definition_required'] = 'A column definition is required for that operation.';
+$lang['db_column_name_required'] = 'требуется имя столбца для этой операции.';
+$lang['db_column_definition_required'] = 'требуется определение столбца для этой операции.';
 $lang['db_unable_to_set_charset'] = 'Unable to set client connection character set: %s';
-$lang['db_error_heading'] = 'A Database Error Occurred';
+$lang['db_error_heading'] = 'Произошла ошибка БД';
 
 /* End of file db_lang.php */
 /* Location: ./system/language/english/db_lang.php */
