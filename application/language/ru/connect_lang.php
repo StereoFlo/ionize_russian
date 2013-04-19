31d9779b72020d6e0f7ad6f5ff20cfff7eb40584
lanuser <lanuser@lanuser.ru>
2013-04-19 15:02:21 +0400

    connect

--- application/language/ru/connect_lang.php
+++ application/language/ru/connect_lang.php
@@ -23,35 +23,35 @@ $lang['connect_register_success'] = 'You have successfully registered.';
 $lang['connect_register_success_verify_user'] = 'You have successfully registered but we must verify your data. An e-mail has been sent to you, please check it and click the activation link in the message to activate your account.';
 
 // Activation mail to Admin
-$lang['connect_admin_mail_subject'] = 'Registration';
-$lang['connect_admin_mail_title'] = 'Registration';
-$lang['connect_admin_mail_intro'] = 'A user just registered to the website.';
-$lang['connect_admin_mail_nom'] = 'Name';
-$lang['connect_admin_mail_login'] = 'Login';
+$lang['connect_admin_mail_subject'] = 'Регистрация';
+$lang['connect_admin_mail_title'] = 'Регистрация';
+$lang['connect_admin_mail_intro'] = 'Зарегистрированный пользователь на сайте.';
+$lang['connect_admin_mail_nom'] = 'Имя';
+$lang['connect_admin_mail_login'] = 'логин';
 $lang['connect_admin_mail_email'] = 'Email';
-$lang['connect_admin_mail_activation_link'] = 'Activation link';
+$lang['connect_admin_mail_activation_link'] = 'Ссылка активации';
 
 // Activation mail to User
-$lang['connect_user_mail_subject'] = 'Your registration';
-$lang['connect_user_mail_activated'] = 'Account activated';
-$lang['connect_act_user_mail_title'] = 'Welcome !';
-$lang['connect_act_user_mail_intro'] = 'You just registered to our website and we thank you.';
-$lang['connect_act_user_mail_text'] = 'To confirm your registration, click on this activation link.';
-$lang['connect_act_user_mail_activation_link'] = 'Activation link';
+$lang['connect_user_mail_subject'] = 'Ваша регистрация';
+$lang['connect_user_mail_activated'] = 'Аккаунт активирован';
+$lang['connect_act_user_mail_title'] = 'Добро пожаловать!';
+$lang['connect_act_user_mail_intro'] = 'Вы зарегистрировались на нашем сайте.';
+$lang['connect_act_user_mail_text'] = 'Чтобы подтвердить вашу регистрацию, нажмите на ссылку активации.';
+$lang['connect_act_user_mail_activation_link'] = 'Ссылка активации';
 
 // Registration confirmation mail to User
-$lang['connect_wait_user_mail_title'] = 'Welcome !';
-$lang['connect_wait_user_mail_intro'] = 'You just registered to our website and we thank you.';
-$lang['connect_wait_user_mail_text'] = 'Your account will be activated by the administrator quickly.';
+$lang['connect_wait_user_mail_title'] = 'Добро пожаловать!';
+$lang['connect_wait_user_mail_intro'] = 'Зарегистрированный пользователь на сайте.';
+$lang['connect_wait_user_mail_text'] = 'Ваш аккаунт будет активирован администратором в ближайшее время.';
 
 // Registration views
-$lang['connect_user_registration_title'] = 'Registration successfull';
-$lang['connect_user_registration_message'] = 'You will get an email with your registration informations and instruction to confirm your registration';
+$lang['connect_user_registration_title'] = 'Регистрация завершена';
+$lang['connect_user_registration_message'] = 'В письме вы получите инструкции по дальнейшей активации аккаунта.';
 
 // Activation views
-$lang['connect_home_page'] = 'Home page';
-$lang['connect_activation_title'] = 'Account activation';
-$lang['connect_user_activated_message'] = 'Your account is activated.<br/>You can connect from the Home page';
+$lang['connect_home_page'] = 'Главная страница';
+$lang['connect_activation_title'] = 'Аккаунт активирован';
+$lang['connect_user_activated_message'] = 'Ваш аккаут активирован.<br/>Вы можете войти с главной страницы';
 $lang['connect_user_activated_error'] = 'Something went wrong with the account you are trying to activate. Maybe have you already activated it, or maybe are you using the wrong informations? Try to login with your account informations, or verify the email we\'ve sent to you and try again.';
 
 $lang['connect_admin_activated_message'] = 'This account is activated now.<br/>A mail was just sent to the user to inform him.';
